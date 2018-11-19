03661654385fedb4b3e789450f8e38b284b59f22
Kurt Bauer <kurtbauer55@protonmail.com>
2018-11-19 16:29:27 -0500

    setup reverse button boilerplate code
    also have added images folder

---
+++ app/components/Button/styles.js
@@ -0,0 +1,8 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default EStyleSheet.create({});
+
+
+// would try to avoid setting styles to
+// touchable elements, inconsistent
+// the way that they work
