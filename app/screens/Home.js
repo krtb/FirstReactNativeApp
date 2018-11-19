45015fae213b0e839983b4da98b676ea67e9d5d2
Kurt Bauer <kurtbauer55@protonmail.com>
2018-11-19 10:54:41 -0500

    setup boilerplate for TextInput component

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { View, StatusBar } from 'react-native'
+import React, { Component } from 'react';
+import { StatusBar } from 'react-native'
+
 import { Container } from '../components/Container'
 import { Logo } from '../components/Logo'
+import { InputWithButton } from '../components/TextInput'
 
-export default () => (
-    <Container>
-        <StatusBar translucent={false} barStyle="light-content" />
-        <Logo /> 
-    </Container>
-); 
+class Home extends Component {
+    render() {
+        return (
+            <Container>
+                <StatusBar translucent={false} barStyle="light-content" />
+                <Logo />
+                <InputWithButton/>
+                <InputWithButton
+                    editable={false}
+                />
+            </Container>
+        );
+    }
+}
+
+export default Home;
