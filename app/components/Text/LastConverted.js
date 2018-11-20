650136763955bbf483f844c4eb6b5d2c20db169d
Kurt Bauer <kurtbauer55@protonmail.com>
2018-11-20 09:05:11 -0500

    Text component boilerplate code setup

---
+++ app/components/Text/LastConverted.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types'; 
+import { Text } from 'react-native';
+import moment from 'moment';
+
+import styles from './styles'
+
+const LastConverted = () => (
+
+);
+
+LastConverted.propTypes = {
+
+}
+
+export default LastConverted;
