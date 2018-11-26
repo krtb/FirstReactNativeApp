4bfbf180dd4284906b6098338a5334a4d08928d6
Kurt Bauer <kurtbauer55@protonmail.com>
2018-11-26 17:35:01 -0500

    Revert "Get rid of warning and wrap in view "
    This reverts commit c95ae828dadf91a614a6ed883e68b9c9ac54440d.

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import { Text, FlatList, View, StatusBar } from 'react-native';
+import { Text, FlatList } from 'react-native';
 
 import currencies from '../data/currencies';
 
 const CurrencyList = () => (
-    // wrap entire thing in a view, multiple items
-    // React Native uses flexbox, would be good to learn this and use in future css
-    <View style={{ flex: 1 }} >
     // data can be a plain array
     <FlatList
         data={currencies}
         // rendering items prop
         // can use array destructuring
         renderItem={({item})=> <Text> {item} </Text> }
-        // because we are only returning strings, key will be the actual item that is being rendered
-        keyExtractor={ item => item }
     />
-    </View>
 );
 
 export default CurrencyList;
