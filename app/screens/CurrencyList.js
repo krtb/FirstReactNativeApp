4c6b0d4e79b096474d4eaabe57ed632006c5cbe1
Kurt Bauer <kurtbauer55@protonmail.com>
2018-11-29 11:28:14 -0500

    added translucent bar

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -5,6 +5,7 @@ import currencies from '../data/currencies';
 
 const CurrencyList = () => (
     <View style={{ flex: 1 }} >
+    <StatusBar translucent={false} barStyle="default" />
     <FlatList
         data={currencies}
         renderItem={({item})=> <Text> {item} </Text> }
