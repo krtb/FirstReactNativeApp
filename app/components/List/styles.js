c46bfe261cf6a1e953e9cd63a129ea572f3cd2e1
Kurt Bauer <kurtbauer55@protonmail.com>
2018-11-29 16:34:04 -0500

    added text and separator styling

--- app/components/List/styles.js
+++ app/components/List/styles.js
@@ -1,3 +1,4 @@
+import StyleSheet from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 const styles = EStyleSheet.create({
@@ -10,8 +11,15 @@ const styles = EStyleSheet.create({
         backgroundColor: '$white',
     },
     text: {
-
+        fontSize: 16,
+        color: '$darkText',
     },
+    separator: {
+        marginLeft: 20,
+        backgroundColor: '$border',
+        flex: 1,
+        height: StyleSheet.hairlineWidth,
+    }
 });
 
 export default styles;
