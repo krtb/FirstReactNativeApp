7fdd277a182d72c27effcc95154dbb692a61650a
Kurt Bauer <kurtbauer55@protonmail.com>
2018-11-29 09:50:14 -0500

    removed inline comments that broke Expo emulator

--- app/index.js
+++ app/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet'
 
-// need to install this package!
 import CurrencyList from './screens/CurrencyList';
 
-// here is where global styles are contained
 EStyleSheet.build({
     $primaryBlue: '#4F6D7A',
     $white: '#FFFFFF',
