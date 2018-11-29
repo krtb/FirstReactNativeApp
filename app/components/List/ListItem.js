616c80a737c1cde2c632e8302620f5a9c5bff4ad
Kurt Bauer <kurtbauer55@protonmail.com>
2018-11-29 16:03:52 -0500

    adding style tags to view and touchable highlight

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types'; // ES6
 import { View, Text, TouchableHighlight } from 'react-native';
 
+import styles from './styles'
+
 const ListItem = ({ text, onPress, selected}) => (
     <TouchableHighlight onPress={onPress} >
-        <View>
-            <Text>{text}</Text>
+        <View style={styles.row} >
+            <Text style={styles.text} >{text}</Text>
             {selected ? <Text> Selected! </Text> : null}
         </View>
     </TouchableHighlight>
