d0b595a4df04205afe410b6499f065b8be0bdd77
Kurt Bauer <kurtbauer55@protonmail.com>
2018-11-29 12:47:43 -0500

    turn functional into class component

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, FlatList, View, StatusBar } from 'react-native';
 
 import { ListItem } from '../components/List';
 import currencies from '../data/currencies';
 
-const CurrencyList = () => (
+const TEMP_CURRENT_CURRENCY = 'CAD';
+
+class CurrencyList extends Component {
+
+    handlePress = () => {
+        console.log('row press');
+    };
+
+    render() {
+        return(
     <View style={{ flex: 1 }} >
     <StatusBar translucent={false} barStyle="default" />
     <FlatList
         data={currencies}
-        renderItem={({item})=> <ListItem/> }
+        renderItem={({item})=> <ListItem
+            text={item}
+            selected={item === TEMP_CURRENT_CURRENCY}
+            onPress={this.handlePress}
+        /> }
         keyExtractor={ item => item }
     />
     </View>
-);
+        );
+    }
+};
 
 export default CurrencyList;
