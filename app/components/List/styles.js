3f2af34273972109990889ea732f7b4a1d2d99a1
Kurt Bauer <kurtbauer55@protonmail.com>
2018-11-29 16:14:19 -0500

    adding styling for the text row

--- app/components/List/styles.js
+++ app/components/List/styles.js
@@ -1,7 +1,17 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 const styles = EStyleSheet.create({
+    row: {
+        paddingHorizontal: 20,
+        paddingVertical: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '$white',
+    },
+    text: {
 
+    },
 });
 
 export default styles;
