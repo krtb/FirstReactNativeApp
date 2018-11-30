2e6422fa7d4c4c6238de4af622dd85802204adbd
Kurt Bauer <kurtbauer55@protonmail.com>
2018-11-30 16:27:42 -0500

    added prop types to Text component of ListItem

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types'; // ES6
 import { View, Text, TouchableHighlight } from 'react-native';
 
-import styles from './styles'
+import styles from './styles';
+import Icon from './Icon';
 
-const ListItem = ({ text, onPress, selected}) => (
+const ListItem = ({ text, onPress, selected=false, checkmark=true, visible=true}) => (
     <TouchableHighlight onPress={onPress} underlayColor={styles.$underlayColor} >
         <View style={styles.row} >
             <Text style={styles.text} >{text}</Text>
-            {selected ? <Text> Selected! </Text> : null}
+            {selected ? <Icon checkmark={checkmark} visible={visible} /> : <Icon/> }
         </View>
     </TouchableHighlight>
 );
@@ -16,7 +17,9 @@ const ListItem = ({ text, onPress, selected}) => (
 ListItem.propTypes = {
     text: PropTypes.string,
     onPress: PropTypes.func,
-    selected: PropTypes.bool
+    selected: PropTypes.bool,
+    checkmark: PropTypes.bool,
+    visible: PropTypes.bool,
 }
 
 export default ListItem;
