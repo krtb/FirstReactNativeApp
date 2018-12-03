5b88f2f0660d024ed6c7e37ccf04beeb029d4d9b
Kurt Bauer <kurtbauer55@protonmail.com>
2018-12-02 21:24:46 -0500

    have imported primary modules to Options component

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,3 +1,8 @@
+import React, { Component } from 'react';
+import { ScrollView, StatusBar } from 'react-native';
+
+import { ListItem, Separator } from '../components/List';
+
 const Options = () => null;
 
 export default Options;
