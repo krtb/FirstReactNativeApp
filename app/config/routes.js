167b7a87d2d85c6a5bb51223febd87e544a11965
Kurt Bauer <kurtbauer55@protonmail.com>
2018-12-04 11:43:36 -0500

    added config folder and setup initial React Navigation to specific modules in app

---
+++ app/config/routes.js
@@ -0,0 +1,13 @@
+import { StackNavigator } from ' react-navivation ';
+
+import Home from '../screens/Home';
+import CurrencyList from '../screens/CurrencyList';
+
+export default StackNavigator({
+    Home: {
+        screen: Home,
+    },
+    CurrencyList: {
+        screen: Home,
+    }, 
+});
