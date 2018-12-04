36aff307454619ec75e5e48fd35a4b9af2b7095c
Kurt Bauer <kurtbauer55@protonmail.com>
2018-12-04 10:29:01 -0500

    added all colors with working callback function and rows

--- app/screens/Themes.js
+++ app/screens/Themes.js
@@ -4,9 +4,16 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 import { ListItem, Separator } from '../components/List';
 
+const styles = EStyleSheet.create({
+    $blue: '$primaryBlue',
+    $green: '$primaryGreen',
+    $orange: '$primaryOrange',
+    $purple: '$primaryPurple',
+});
+
 class Themes extends Component {
-    handleThemePress = () => {
-        console.log('pressed theme');
+    handleThemePress = (color) => {
+        console.log('pressed theme', color);
     }
 
     render() {
@@ -15,8 +22,32 @@ class Themes extends Component {
                 <StatusBar translucent={false} barStyle='default' />
                 <ListItem
                     text="Blue"
-                    handleThemePress={this.handleThemePress}
+                    onPress={ () => this.handleThemePress(styles.$blue)}
+                    selected
+                    checkmark={false}
                 />
+                <Separator/>
+                <ListItem
+                    text="Orange"
+                    onPress={() => this.handleThemePress(styles.$orange)}
+                    selected
+                    checkmark={false}
+                />
+                <Separator />
+                <ListItem
+                    text="Green"
+                    onPress={() => this.handleThemePress(styles.$green)}
+                    selected
+                    checkmark={false}
+                />
+                <Separator />
+                <ListItem
+                    text="Purple"
+                    onPress={() => this.handleThemePress(styles.$purple)}
+                    selected
+                    checkmark={false}
+                />
+                <Separator />
             </ScrollView>
         );
     }
