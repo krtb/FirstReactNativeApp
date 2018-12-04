4597591ce2b944ab639597f2d244399e03f7b1cb
Kurt Bauer <kurtbauer55@protonmail.com>
2018-12-04 11:01:02 -0500

    have all colors properly displaying on Theme screen

--- app/screens/Themes.js
+++ app/screens/Themes.js
@@ -6,51 +6,54 @@ import { ListItem, Separator } from '../components/List';
 
 const styles = EStyleSheet.create({
     $blue: '$primaryBlue',
-    $green: '$primaryGreen',
     $orange: '$primaryOrange',
+    $green: '$primaryGreen',
     $purple: '$primaryPurple',
 });
 
 class Themes extends Component {
-    handleThemePress = (color) => {
-        console.log('pressed theme', color);
-    }
+    handlePressTheme = () => {
+        console.log('press theme');
+    };
 
     render() {
         return (
             <ScrollView>
-                <StatusBar translucent={false} barStyle='default' />
+                <StatusBar translucent={false} barStyle="default" />
                 <ListItem
                     text="Blue"
-                    onPress={ () => this.handleThemePress(styles.$blue)}
+                    onPress={() => this.handlePressTheme(styles.$blue)}
                     selected
                     checkmark={false}
+                    iconBackground={styles.$blue}
                 />
-                <Separator/>
+                <Separator />
                 <ListItem
                     text="Orange"
-                    onPress={() => this.handleThemePress(styles.$orange)}
+                    onPress={() => this.handlePressTheme(styles.$orange)}
                     selected
                     checkmark={false}
+                    iconBackground={styles.$orange}
                 />
                 <Separator />
                 <ListItem
                     text="Green"
-                    onPress={() => this.handleThemePress(styles.$green)}
+                    onPress={() => this.handlePressTheme(styles.$green)}
                     selected
                     checkmark={false}
+                    iconBackground={styles.$green}
                 />
                 <Separator />
                 <ListItem
                     text="Purple"
-                    onPress={() => this.handleThemePress(styles.$purple)}
+                    onPress={() => this.handlePressTheme(styles.$purple)}
                     selected
                     checkmark={false}
+                    iconBackground={styles.$purple}
                 />
                 <Separator />
             </ScrollView>
         );
     }
 }
-
 export default Themes;
