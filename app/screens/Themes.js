f531fcb518f83c858cef96de657f859806974ecc
Kurt Bauer <kurtbauer55@protonmail.com>
2018-12-04 10:18:33 -0500

    added text and callback function to Themes screen view

--- app/screens/Themes.js
+++ app/screens/Themes.js
@@ -1,3 +1,25 @@
-const Themes = () => null;
+import React, { Component } from 'react';
+import { ScrollView, StatusBar } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+import { ListItem, Separator } from '../components/List';
+
+class Themes extends Component {
+    handleThemePress = () => {
+        console.log('pressed theme');
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <StatusBar translucent={false} barStyle='default' />
+                <ListItem
+                    text="Blue"
+                    handleThemePress={this.handleThemePress}
+                />
+            </ScrollView>
+        );
+    }
+}
 
 export default Themes;
