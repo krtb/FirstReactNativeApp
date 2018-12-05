b2b235b525c8c46fbe4176c0b0ffb18726e6e99f
Kurt Bauer <kurtbauer55@protonmail.com>
2018-12-05 14:38:58 -0500

    created initial switch statement for reducer

---
+++ app/reducers/currencies.js
@@ -0,0 +1,20 @@
+import { CHANGE_CURRENCY_AMOUNT, SWAP_CURRENCY } from '../actions/reducers';
+// can use dif actio types in reducer, based off of which action type
+// which reducer function should we actually run.
+
+const initialState = {};
+
+// after each subsequent action it called, it will have the most recent state 
+// as each action is called, they will go ahead and modify the state
+
+// switch statement on 'action.type' because each action going out will be unique 
+const reducer = (state, action) => {
+    switch(action.type){
+        case CHANGE_CURRENCY_AMOUNT:
+        case SWAP_CURRENCY:
+        default:
+            return state;
+    }
+}
+
+export default reducer;
