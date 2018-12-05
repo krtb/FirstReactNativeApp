7e673dacca907170a2b80581083c4ac737f0334f
Kurt Bauer <kurtbauer55@protonmail.com>
2018-12-05 10:11:31 -0500

    imported Linking module from react-native

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView, StatusBar, Platform } from 'react-native';
+import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { ListItem, Separator } from '../components/List';
 
