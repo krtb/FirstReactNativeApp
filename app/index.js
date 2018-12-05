0b271018008f73371df9874b8dd989747a08e4dc
Kurt Bauer <kurtbauer55@protonmail.com>
2018-12-05 11:17:10 -0500

    added in AlertProvider code

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet'
 
-import Navigator from './config/routes'; 
+import Navigator from './config/routes';
+import {AlertProvider} from './components/Alert'
 
 EStyleSheet.build({
     $primaryBlue: '#4F6D7A',
