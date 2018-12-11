1741b8082dd3114fc4bb2cd8bc43b61fb3db6b4e
Kurt Bauer <kurtbauer55@protonmail.com>
2018-12-11 16:11:41 -0500

    added comments to code

--- app/config/store.js
+++ app/config/store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+// can use logging to make sure that our redux state is updating correctly
 
 import reducer from '../reducers';
 
+// check makes sure that this middleware only works in a development environment
 const middleware = [];
 if (process.env.NODE_ENV === 'development') {
     middleware.push(logger);
