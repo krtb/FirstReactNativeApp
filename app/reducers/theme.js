9e1a4d58b4e138d88611831942f0da71f7e2e4e0
Kurt Bauer <kurtbauer55@protonmail.com>
2018-12-13 15:44:59 -0500

    have our reducer function setup to change theme color on touch

---
+++ app/reducers/theme.js
@@ -0,0 +1,15 @@
+import { CHANGE_PRIMARY_COLOR } from '../actions/theme';
+
+const initialState = {
+    // this property is unique to this initial state,
+    // aligns with the color we've been using: $primaryBlue
+    primaryColor: '#4F6D7A',
+};
+
+export default (state = initialState, action ) => {
+    switch (action.type) {
+        case CHANGE_PRIMARY_COLOR:
+        default:
+            return state;
+    }
+}
